778c329d0390fc9d2cc5e5b7bd51a87cf6017182
macheuz <machadosmatheus.0@gmail.com>
2023-03-15 21:51:58 -0300

    teste&

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
+<?php
+$emailTo = '<administrativo@segtaxi.com.br>';
+$show_modal = false;
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name    = stripslashes(trim($_POST['name']));
+    $email   = stripslashes(trim($_POST['email']));
+    $phone   = stripslashes(trim($_POST['phone']));
+    $message = stripslashes(trim($_POST['message']));
 
+    $emailIsValid = preg_match('/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/', $email);
+
+    if ($name && $email && $emailIsValid && $phone && $message) {
+        $body = "Nome: $name <br /> Email: $email <br /> Telefone: $phone <br /> Mensagem: $message";
+
+        $headers  = 'MIME-Version: 1.1' . PHP_EOL;
+        $headers .= 'Content-type: text/html; charset=utf-8' . PHP_EOL;
+        $headers .= "From: $name <$email>" . PHP_EOL;
+        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        $headers .= "Reply-To: $email" . PHP_EOL;
+		
+        mail($emailTo, "[Contato via Site]", $body, $headers);
+        $emailSent = true;
+	$show_modal = true;
+    } else {
+        $hasError = true;
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
